f9ed20b0bfb1c8263b346cd41034f16db8a9b03d
3nityGitHub <60592651+3nityGitHub@users.noreply.github.com>
2022-10-19 11:22:55 +0100

    Modify HTML, CSS and JS files

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let hamburgerIcon = document.querySelector('.hamburger_icon');
 let navMenu = document.querySelector('.nav_menu');
+let navBranding = document.querySelector('.nav_branding');
 
 hamburgerIcon.addEventListener('click', () => {
   hamburgerIcon.classList.toggle('active');
   navMenu.classList.toggle('active');
+  navBranding.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav_link').forEach((n) =>
   n.addEventListener('click', () => {
     hamburgerIcon.classList.remove('active');
     navMenu.classList.remove('active');
+    navBranding.classList.remove('active');
   })
 );
